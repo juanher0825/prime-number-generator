93382c9b6072865cc0b7d9bb3797e19287ef13fa
Juan Hernandez <juanher0825@gmail.com>
2020-02-13 23:46:00 -0500

    A little change in the Requiriments and start to create the Diagram Class, also adjust the folders in the repository. First pull

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Prime number generator");
 			primaryStage.show();
