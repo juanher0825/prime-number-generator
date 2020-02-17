3d2af01a7d9897d6b751a6e5e066277904555f81
cadavinci <acam97@hotmail.com>
2020-02-16 22:51:20 -0500

    painting the cells

--- src/threads/PaintingField.java
+++ src/threads/PaintingField.java
@@ -4,47 +4,75 @@ import ui.MatrixVisualizerWindowController;
 import exceptions.OffTheLimitException;
 import model.*;
 
-public class PaintingField extends Thread{
+public class PaintingField extends Thread {
 
 	private MatrixVisualizerWindowController mv;
-	
+
 	private Generator a;
-	
+
 	private int contador;
 	
-	
-	public PaintingField(MatrixVisualizerWindowController mv, int n) throws OffTheLimitException {
+	private int option;
+
+	public PaintingField(MatrixVisualizerWindowController mv, int n , int o) throws OffTheLimitException {
 		this.mv = mv;
 		contador = 0;
+		option = o;
 		a = new Generator(n);
 	}
+
+	
 	
 	public void run() {
-		System.out.println("Entra al Hilo" + " " + contador + mv.getN());
-		while(contador <= mv.getN()) {
-			System.out.println(contador);
-			mv.paint(a.betwiseSieve(mv.getN()), mv.getButons());
-			try {
-				sleep(2000);
-			}catch (InterruptedException e) {
-				e.printStackTrace();
-			
-		}
+		if (option == 1) {
+			System.out.println("Entra al Hilo" + " " + contador + mv.getN());
+			while (contador <= mv.getN()) {
+
+				try {
+					System.out.println(contador);
+					mv.paint(a.betwiseSieve(mv.getN()), mv.getButons());
+					sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+
+				}
+			}
+		}else if (option == 2) {
+			System.out.println("Entra al Hilo" + " " + contador + mv.getN());
+			while (contador <= mv.getN()) {
+
+				try {
+					System.out.println(contador);
+					mv.paint(a.SieveOfSundaram(mv.getN()), mv.getButons());
+					sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+
+				}
+			}
+		}else if (option ==3) {
+			System.out.println("Entra al Hilo" + " " + contador + mv.getN());
+			while (contador <= mv.getN()) {
+
+				try {
+					System.out.println(contador);
+					mv.paint(a.sieveOfEratosthenes(mv.getN()), mv.getButons());
+					sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+
+				}
+			}
 		}
 	}
-	
-	/*public void run() {
-		while(c) {
-			r = mc.rigth(r);
-			r = mc.left(r);
-			
-			try {
-				sleep(50);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			
-		}*/
+
+	/*
+	 * public void run() { while(c) { r = mc.rigth(r); r = mc.left(r);
+	 * 
+	 * try { sleep(50); } catch (InterruptedException e) { // TODO Auto-generated
+	 * catch block e.printStackTrace(); }
+	 * 
+	 * 
+	 * }
+	 */
 }
--- src/ui/MatrixVisualizerWindowController.java
+++ src/ui/MatrixVisualizerWindowController.java
@@ -1,4 +1,5 @@
 package ui;
+
 import model.*;
 import threads.PaintingField;
 
@@ -25,180 +26,201 @@ public class MatrixVisualizerWindowController {
 	private int rows = 0;
 	private int columns = 0;
 	private int n = 0;
-	
+
 	private int contador;
-	
+
 	private Generator g;
-	
+
 	private PaintingField pf;
-	
-	
-	
+
 	@FXML
 	private BorderPane pane;
 
-    @FXML
-    private Button Prime1_btn;
+	@FXML
+	private Button Prime1_btn;
 
-    @FXML
-    private Button Prime2_btn;
+	@FXML
+	private Button Prime2_btn;
 
-    @FXML
-    private Button Prime3_btn;
+	@FXML
+	private Button Prime3_btn;
 
-    @FXML
-    private Label Label_dimension;
+	@FXML
+	private Label Label_dimension;
 
-    @FXML
-    private Button Back_btn;
-    
-    private GridPane grid;
-    private Button[][] butons;
-    
-  //Set
-    public void setDimenssions(int c ,int  r, int n){
-    	this.n = n;
-    	columns = c;
-    	rows = r;
-    	butons = new  Button [r][c];
-    	showFields();
-    }
-    
-    
-    //this function displays the matrix of numbers
-    //starting in 1 until n.
-    //Creates a Gridpane dinamically and fills it 
-    //up with buttons
-    public void showFields() {
-    	
-    	int [][] fields =new int[rows][columns];
-		grid=new GridPane();
+	@FXML
+	private Button Back_btn;
+
+	private GridPane grid;
+	private Button[][] butons;
+
+	// Set
+	public void setDimenssions(int c, int r, int n) {
+		this.n = n;
+		columns = c;
+		rows = r;
+		butons = new Button[r][c];
+		showFields();
+	}
+
+	// this function displays the matrix of numbers
+	// starting in 1 until n.
+	// Creates a Gridpane dinamically and fills it
+	// up with buttons
+	public void showFields() {
+
+		int[][] fields = new int[rows][columns];
+		grid = new GridPane();
 		pane.setCenter(grid);
-		
-		//filler is a variable that is used
-		//to set the text of each one of the
-		//buttons within the matrix
-		int filler =1;
-		
-		for(int x=0; x<fields.length; x++){//Rows
-			RowConstraints row=new RowConstraints();
+
+		// filler is a variable that is used
+		// to set the text of each one of the
+		// buttons within the matrix
+		int filler = 1;
+
+		for (int x = 0; x < fields.length; x++) {// Rows
+			RowConstraints row = new RowConstraints();
 			row.setVgrow(Priority.ALWAYS);
 			grid.getRowConstraints().add(row);
 		}
-		for(int y=0; y<fields[0].length; y++){//Columns
-			ColumnConstraints column=new ColumnConstraints();
+		for (int y = 0; y < fields[0].length; y++) {// Columns
+			ColumnConstraints column = new ColumnConstraints();
 			column.setHgrow(Priority.ALWAYS);
 			grid.getColumnConstraints().add(column);
 		}
-		
-		for(int x=0; x<fields.length; x++){
-			for(int y=0; y<fields[0].length; y++){
-				int i=x;
-				int j=y;
-				Button field=new Button();
+
+		for (int x = 0; x < fields.length; x++) {
+			for (int y = 0; y < fields[0].length; y++) {
+				int i = x;
+				int j = y;
+				Button field = new Button();
 				field.setMaxWidth(Double.MAX_VALUE);
 				field.setMaxHeight(Double.MAX_VALUE);
-				if (filler<=n) {
-					field.setText(filler+"");
-				}else {
+				if (filler <= n) {
+					field.setText(filler + "");
+				} else {
 					field.setText("");
 				}
-				
-				butons [x][y] = field;
-			
+
+				butons[x][y] = field;
 
 				grid.add(butons[x][y], y, x);
-				
+
 				filler++;
 			}
 		}
-		
-    }
 
-    @FXML
-    void Back(ActionEvent event) {
-    	try {
-    		System.out.println("entraaaa");
-			FXMLLoader loader=new FXMLLoader(getClass().getResource("MainWindow.fxml"));
-			Parent root = (Parent) loader.load();
-			root.getStylesheets().add("application.css");//CSS
-			
-			MainWindowController nextController=loader.getController();
-			
-			
-			Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-			stage.setScene(new Scene(root));
-			
-		
-		}
-		catch (Exception ex) {
-			ex.printStackTrace();
-		}
-    }
-    
-    public int getN() {
-    	return n;
-    }
-    
-
-	
-
-    @FXML
-    void Prime1(ActionEvent event) throws OffTheLimitException {
-    	
-    	pf = new PaintingField(this, getN());
-    	
-    	pf.start();
-
-    }
-
-    @FXML
-    void Prime2(ActionEvent event) {
-    	//butons[0][0].setStyle("-fx-background-color: Red");
-    }
-
-    @FXML
-    void Prime3(ActionEvent event) {
-
-    }
-    
-    public void recibirscene(Generator a) {
-    	g = a;
-    }
-    
-    public Button[][] getButons() {
-		return butons;
 	}
 
+	@FXML
+	void Back(ActionEvent event) {
+		try {
+			System.out.println("entraaaa");
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+			Parent root = (Parent) loader.load();
+			root.getStylesheets().add("application.css");// CSS
+
+			MainWindowController nextController = loader.getController();
+
+			Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			stage.setScene(new Scene(root));
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public int getN() {
+		return n;
+	}
+
+	//bitwise sieve
+	@FXML
+	void Prime1(ActionEvent event) throws OffTheLimitException {
+
+		pf = new PaintingField(this, getN(),1);
+
+		pf.start();
+
+	}
+
+	//sieve of sundaram
+	@FXML
+	void Prime2(ActionEvent event) throws OffTheLimitException {
+		// butons[0][0].setStyle("-fx-background-color: Red");
+		pf = new PaintingField(this, getN(),2);
+
+		pf.start();
+	}
+
+	//sieve of eratosthenes
+	@FXML
+	void Prime3(ActionEvent event) throws OffTheLimitException {
+		pf = new PaintingField(this, getN(),3);
+
+		pf.start();
+	}
+
+	public void recibirscene(Generator a) {
+		g = a;
+	}
+
+	public Button[][] getButons() {
+		return butons;
+	}
 
 	public void setButons(Button[][] butons) {
 		this.butons = butons;
 	}
 
-
+//	public void paint(ArrayList<Integer> a, Button z[][]) {
+//		// button.setStyle("-fx-background-color: #ff0000; ");
+//
+//		for (int i = 0; i <= a.size() - 1; i++) {
+//			for (int j = 0; j < z.length; j++) {
+//				for (int k = 0; k < z[j].length; k++) {
+//					if (Integer.parseInt(z[j][k].getText()) == a.get(i)) {
+//						System.out.println(a.get(i));
+//						System.out.println(z[j][k].getText());
+//						butons[j][k].setStyle("-fx-background-color: Green");
+//					} else {
+//						System.out.println("rojo");
+//						System.out.println(a.get(i));
+//						System.out.println(z[j][k].getText());
+//						System.out.println();
+//						butons[j][k].setStyle("-fx-background-color: Red");
+//					}
+//				}
+//			}
+//		}
+//
+//	}
+	
+	
 	public void paint(ArrayList<Integer> a, Button z[][]) {
-    	//button.setStyle("-fx-background-color: #ff0000; ");
-    	
-    	for(int i = 0; i<=a.size()-1; i++) {
-    		for (int j = 0; j < z.length; j++) {
-				for (int k = 0; k < z[j].length; k++) {
-					if(Integer.parseInt(z[j][k].getText()) == a.get(i)) {
-						System.out.println("");
-						butons[j][k].setStyle("-fx-background-color: Green");
+		// button.setStyle("-fx-background-color: #ff0000; ");
+		int count = 0;
+		for (int i = 0; i <z.length;i++) {
+			for (int j = 0; j<z[0].length;j++) {
+				try {
+					if (Integer.parseInt(z[i][j].getText()) == a.get(count)) {
+						butons[i][j].setStyle("-fx-background-color: Green");
+						count++;
+					}else {
+						butons[i][j].setStyle("-fx-background-color: Red");
 					}
+				}catch(IndexOutOfBoundsException e) {
+					butons[i][j].setStyle("-fx-background-color: Red");
+					continue;
 				}
 			}
-    	}
-    	
-    }
-    
-    
-    @FXML
-    
-    void initialize() {
-    }
-    
-    
+		}
+		
+	}
 
+	@FXML
+
+	void initialize() {
+	}
 
 }
